f5986141119e51ea67bd278f9c0d85733c90afb4
Bhavika Jain <26361255+bhavikajain0001@users.noreply.github.com>
2023-05-19 18:09:18 +0530

    Update REEdI.py

--- REEdI.py
+++ REEdI.py
@@ -101,34 +101,42 @@ def main():
 		with col2:
 			st.markdown('<p style="font-family:Roboto;font-size:60px;font-weight:bold">🎓 Skills-In-Demand Analysis </p>', unsafe_allow_html=True)
 		html_temp = """
-			<div class='tableauPlaceholder' id='viz1683884933680' style='position: relative'>
-				<noscript>
-					<a href='#'><img alt='REEdI Modules&#39; Skillification ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;RE&#47;REEdICoursesSkillification&#47;REEdICoursesSkillification&#47;1_rss.png' style='border: none' /></a>
-				</noscript>
-				<object class='tableauViz' style='display:none;'>
-					<param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
-					<param name='embed_code_version' value='3' />
-					<param name='site_root' value='' />
-					<param name='name' value='REEdICoursesSkillification&#47;REEdICoursesSkillification' />
-					<param name='tabs' value='no' />
-					<param name='toolbar' value='yes' />
-					<param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;RE&#47;REEdICoursesSkillification&#47;REEdICoursesSkillification&#47;1.png' />
-					<param name='animate_transition' value='yes' />
-					<param name='display_static_image' value='yes' />
-					<param name='display_spinner' value='yes' />
-					<param name='display_overlay' value='yes' />
-					<param name='display_count' value='yes' />
-					<param name='language' value='en-GB' />
-				</object>
+			<div class='tableauPlaceholder' id='viz1684495388691' style='position: relative'>
+			<noscript>
+				<a href='#'><img alt='Demand - MTU ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;De&#47;Demand-MTU&#47;Demand-MTU&#47;1_rss.png' style='border: none' /></a>
+			</noscript>
+			<object class='tableauViz' style='display:none;'>
+				<param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
+				<param name='embed_code_version' value='3' />
+				<param name='site_root' value='' />
+				<param name='name' value='Demand-MTU&#47;Demand-MTU' />
+				<param name='tabs' value='no' />
+				<param name='toolbar' value='yes' />
+				<param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;De&#47;Demand-MTU&#47;Demand-MTU&#47;1.png' />
+				<param name='animate_transition' value='yes' />
+				<param name='display_static_image' value='yes' />
+				<param name='display_spinner' value='yes' />
+				<param name='display_overlay' value='yes' />
+				<param name='display_count' value='yes' />
+				<param name='language' value='en-GB' />
+			</object>
 			</div>
 			<script type='text/javascript'>
-			var divElement = document.getElementById('viz1683884933680');
-			var vizElement = divElement.getElementsByTagName('object')[0];
-			vizElement.style.width = '100%';
-			vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px';
-			var scriptElement = document.createElement('script');
-			scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
-			vizElement.parentNode.insertBefore(scriptElement, vizElement);
+				var divElement = document.getElementById('viz1684495388691');
+				var vizElement = divElement.getElementsByTagName('object')[0];
+				if(divElement.offsetWidth > 800) {
+					vizElement.style.width = '1204px';
+					vizElement.style.height = '1835px';
+				} else if(divElement.offsetWidth > 500) {
+					vizElement.style.width = '1204px';
+					vizElement.style.height = '1835px';
+				} else {
+					vizElement.style.width = '100%';
+					vizElement.style.height = '1827px';
+				}
+				var scriptElement = document.createElement('script');
+				scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+				vizElement.parentNode.insertBefore(scriptElement, vizElement);
 			</script>
 			"""
 		components.html(html_temp, width=1400, height=1000)

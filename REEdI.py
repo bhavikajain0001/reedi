9903c454837b5c27ef465a91e18a1586c3498e3c
Bhavika Jain <26361255+bhavikajain0001@users.noreply.github.com>
2023-05-19 18:04:48 +0530

    Update REEdI.py

--- REEdI.py
+++ REEdI.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 from PIL import Image
 
-st.set_page_config(page_title="REEdI Career Pathway Planning Platform", page_icon="💡", layout="wide")
+st.set_page_config(page_title="💡 REEdI Career Pathway Planning Platform", page_icon="💡", layout="wide")
 overall_streamlit_style = """
 			<style>		
 			@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
@@ -100,11 +100,42 @@ def main():
 			st.image(image_1, use_column_width=True)
 		with col2:
 			st.markdown('<p style="font-family:Roboto;font-size:60px;font-weight:bold">🎓 Skills-In-Demand Analysis </p>', unsafe_allow_html=True)
-		st.subheader("""Coming Soon...""")
+		html_temp = """
+			<div class='tableauPlaceholder' id='viz1683884933680' style='position: relative'>
+				<noscript>
+					<a href='#'><img alt='REEdI Modules&#39; Skillification ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;RE&#47;REEdICoursesSkillification&#47;REEdICoursesSkillification&#47;1_rss.png' style='border: none' /></a>
+				</noscript>
+				<object class='tableauViz' style='display:none;'>
+					<param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
+					<param name='embed_code_version' value='3' />
+					<param name='site_root' value='' />
+					<param name='name' value='REEdICoursesSkillification&#47;REEdICoursesSkillification' />
+					<param name='tabs' value='no' />
+					<param name='toolbar' value='yes' />
+					<param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;RE&#47;REEdICoursesSkillification&#47;REEdICoursesSkillification&#47;1.png' />
+					<param name='animate_transition' value='yes' />
+					<param name='display_static_image' value='yes' />
+					<param name='display_spinner' value='yes' />
+					<param name='display_overlay' value='yes' />
+					<param name='display_count' value='yes' />
+					<param name='language' value='en-GB' />
+				</object>
+			</div>
+			<script type='text/javascript'>
+			var divElement = document.getElementById('viz1683884933680');
+			var vizElement = divElement.getElementsByTagName('object')[0];
+			vizElement.style.width = '100%';
+			vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px';
+			var scriptElement = document.createElement('script');
+			scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+			vizElement.parentNode.insertBefore(scriptElement, vizElement);
+			</script>
+			"""
+		components.html(html_temp, width=1400, height=1000)
 
 def footer():
-    st.markdown('<div style=font-size:15px>Powered by Abodoo</div>', unsafe_allow_html=True)
-    #image_2 = Image.open('/app/images/logo_2.png')
+    st.markdown('<div style=font-size:15px>Powered by </div>', unsafe_allow_html=True)
+    image_2 = Image.open('/app/images/logo_2.png')
     #st.image(image_2, use_column_width=True)
     
 # the controller

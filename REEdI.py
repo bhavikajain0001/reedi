8a079a7ccdd371954b3ba54c5f350e6aee3d7cc3
Bhavika Jain <26361255+bhavikajain0001@users.noreply.github.com>
2023-10-06 15:40:45 +0100

    Update REEdI.py

--- REEdI.py
+++ REEdI.py
@@ -121,7 +121,39 @@ def main():
                 '<p style="font-family:Roboto;font-size:40px;font-weight:bold"> 🎓 REEdI Modules and UNSDGs Mapping Analysis </p>',
                 unsafe_allow_html=True,
             )
-        html_temp = """<div class='tableauPlaceholder' id='viz1696546031267' style='position: relative'><noscript><a href='#'><img alt='      REEdI Modules and UNSDGs Mapping ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;RE&#47;REEdICoursesandUNSDGsMappingAnalysis&#47;Story1&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='REEdICoursesandUNSDGsMappingAnalysis&#47;Story1' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;RE&#47;REEdICoursesandUNSDGsMappingAnalysis&#47;Story1&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-GB' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1696546031267');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>"""
+        html_temp = """
+			 <div class='tableauPlaceholder' id='viz1696603052493' style='position: relative'>
+			  <noscript>
+			    <a href='#'>
+			      <img alt='      REEdI Modules and UNSDGs Mapping ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;RE&#47;REEdICoursesandUNSDGsMappingAnalysis&#47;Story1&#47;1_rss.png' style='border: none' />
+			    </a>
+			  </noscript>
+			  <object class='tableauViz' style='display:none;'>
+		    <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
+		    <param name='embed_code_version' value='3' />
+		    <param name='site_root' value='' />
+		    <param name='name' value='REEdICoursesandUNSDGsMappingAnalysis&#47;Story1' />
+		    <param name='tabs' value='no' />
+		    <param name='toolbar' value='yes' />
+		    <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;RE&#47;REEdICoursesandUNSDGsMappingAnalysis&#47;Story1&#47;1.png' />
+		    <param name='animate_transition' value='yes' />
+		    <param name='display_static_image' value='yes' />
+		    <param name='display_spinner' value='yes' />
+		    <param name='display_overlay' value='yes' />
+		    <param name='display_count' value='yes' />
+		    <param name='language' value='en-GB' />
+		  </object>
+		</div>
+		<script type='text/javascript'>
+		  var divElement = document.getElementById('viz1696603052493');
+		  var vizElement = divElement.getElementsByTagName('object')[0];
+		  vizElement.style.width = '100%';
+		  vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px';
+		  var scriptElement = document.createElement('script');
+		  scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+		  vizElement.parentNode.insertBefore(scriptElement, vizElement);
+		</script>	
+ 	"""
         components.html(html_temp, width=1400, height=900)
 
     elif page == "Skills-In-Demand Analysis":
@@ -179,7 +211,6 @@ def footer():
     st.markdown(
         "<div style=font-size:15px>Powered by Abodoo</div>", unsafe_allow_html=True
     )
-
 
 # the controller
 def load_page():
